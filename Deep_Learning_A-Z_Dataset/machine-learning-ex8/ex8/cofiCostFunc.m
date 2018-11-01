dc9c5f2ad0d986d13d242cb94360e9fac3091a8b
Kevin <jiahuan.h@ciktel.com>
2018-11-01 11:58:07 -0400

    ex-8 done

--- Deep_Learning_A-Z_Dataset/machine-learning-ex8/ex8/cofiCostFunc.m
+++ Deep_Learning_A-Z_Dataset/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -11,7 +11,7 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-            
+
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -40,9 +40,11 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+J = (1/2)*sum(sum((((X * Theta' - Y).* R).^2))) + (lambda/2)*sum(sum(Theta.^2)) + (lambda/2)*sum(sum(X.^2));
 
+X_grad = ((X * Theta' - Y).* R) * Theta + (lambda*X);
 
-
+Theta_grad = (X' * ((X * Theta' - Y).* R))' + (lambda*Theta);
 
 
 
